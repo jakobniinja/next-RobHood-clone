2e0c4088a6a664dd2b0c1cc472f66210ca22498f
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2022-04-03 19:36:55 +0200

    day 2: header -wip

---
+++ components/Header.js
@@ -0,0 +1,28 @@
+import React from "react";
+import image from "next/image";
+import { AiOutlineSearch } from "react-icons/ai";
+
+const styles = {
+  header: "",
+};
+
+const Header = () => {
+  return (
+    <div className={container}>
+      <div className={leftHeader}>{/* <Image/> */}</div>
+      <div className={searchWrapper}>
+        <div className={searchInputContainer}>
+
+            
+        <AiOutlineSearch className={searchIcon} />
+        <div className="searchInputWrapper">
+            <input  placeholder="Search.." className={styles.searchInput} />
+        </div>
+        
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
